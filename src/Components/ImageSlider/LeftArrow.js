05f619fcec91a0e01eef4c50e4e6debc7d72f2bb
T-Streets <tyler.ceja5@gmail.com>
2018-09-07 16:35:30 -0700

    manages slider positioning to better see page

--- src/Components/ImageSlider/LeftArrow.js
+++ src/Components/ImageSlider/LeftArrow.js
@@ -3,6 +3,7 @@ import React from 'react';
 const LeftArrow = (props) => {
     return (
         <div className='prevArrow' onClick={props.prevSlide}>
+            clickLeft
         </div>
     );
 }
--- src/Components/ImageSlider/RightArrow.js
+++ src/Components/ImageSlider/RightArrow.js
@@ -3,6 +3,7 @@ import React from 'react';
 const RightArrow = (props) => {
     return (
         <div className='nextArrow' onClick={props.nextSlide}>
+        clickRight
         </div>
     );
 }
